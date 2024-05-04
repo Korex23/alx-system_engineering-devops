07a7c367b0bc719ea0f25332191658dc39a97846
Korode Okesanya <105870563+Korex23@users.noreply.github.com>
2024-05-04 09:04:41 +0100

    Update 6-phone_number.rb

--- 0x06-regular_expressions/6-phone_number.rb
+++ 0x06-regular_expressions/6-phone_number.rb
@@ -1,2 +1,12 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/^\d{10}$/).join
+
+# Extract the first argument passed in the command line
+phone_number = ARGV[0]
+
+# Regular expression to match a 10-digit phone number
+pattern = /^\d{10}$/
+
+# Check if the phone number matches the pattern
+if phone_number.match?(pattern)
+  puts phone_number
+end
